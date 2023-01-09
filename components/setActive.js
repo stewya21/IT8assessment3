8f2d290ed56d4a8a4db4d06203a97be9f19f761e
dangoodie <70499319+dangoodie@users.noreply.github.com>
2023-01-10 00:16:34 +1100

    solving active issue with github

--- components/setActive.js
+++ components/setActive.js
@@ -1,6 +1,7 @@
 // set active for current page
 jQuery(function ($) {
   var path = window.location.href;
+  console.log(path);
   // because the 'href' property of the DOM element is the absolute path
   $(".nav-link").each(function () {
     if (this.href === path) {
