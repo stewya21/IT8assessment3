76618dd01c6a570133121940c1091497d6a2c1cd
dangoodie <70499319+dangoodie@users.noreply.github.com>
2023-01-10 00:09:32 +1100

    setActive.js implemented

---
+++ components/setActive.js
@@ -0,0 +1,17 @@
+// set active for current page
+jQuery(function ($) {
+  var path = window.location.href;
+  // because the 'href' property of the DOM element is the absolute path
+  $(".nav-link").each(function () {
+    if (this.href === path) {
+      $(this).addClass("active");
+    }
+  });
+});
+
+// change active on click
+
+$(".nav-link").on("click", function () {
+  $(".nav-link").removeClass("active");
+  $(this).addClass("active");
+});
